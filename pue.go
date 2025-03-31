a46ce9b5009cfadaaa52d8775330783d8e282233
K_ <119788148+K0210@users.noreply.github.com>
2025-03-31 10:37:08 +0900

    Update pue.go

--- pue.go
+++ pue.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var str string
+	var a string
+	var s string
 	fmt.Scan(&a) // データを格納する変数のアドレスを指定
 	s := "ぷえぷえ体操いくぷえよ～！  （∩ ๑•﹏•๑｀)ぷえ〜っwwwぷえぷえぷえええ〜っwww (∩ ๑•﹏•๑｀⊃)ぷぇっ～///ぷぇっ～/// (⊂ ๑•﹏•๑｀∩) ぷえええええーーーっ！！(∩ ๑•﹏•๑｀∩)ぷえぷえーっ！！"
-  fmt.Println((strings.Replace(s,"ぷえ",a,-1))
+	fmt.Println((strings.Replace(s,"ぷえ",a,-1))
 }
